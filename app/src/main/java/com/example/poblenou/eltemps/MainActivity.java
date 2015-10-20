7113657b5c8bfaa9ea00a7bc0e0ddc3bb7d23c73
uri <48089748z@iespoblenou.org>
2015-10-20 18:15:14 +0200

    Primer commit del Projecte  "Weather"  fet per mi (Oriol) Mostra el temps de tota la setmana sense agafar les dades d'internet.
    menu_fragment.xml creat.
    
    Tot Funcionant sense errors.

--- app/src/main/java/com/example/poblenou/eltemps/MainActivity.java
+++ app/src/main/java/com/example/poblenou/eltemps/MainActivity.java
@@ -8,24 +8,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
--- app/src/main/java/com/example/poblenou/eltemps/MainActivityFragment.java
+++ app/src/main/java/com/example/poblenou/eltemps/MainActivityFragment.java
@@ -3,20 +3,88 @@ package com.example.poblenou.eltemps;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment
+{
+    ArrayAdapter<String> myAdapter;
+    private ArrayList<String> items;
+    private ListView miLista;
+    private TextView misDias;
 
-    public MainActivityFragment() {
+    public MainActivityFragment()
+    {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View fragmento = inflater.inflate(R.layout.fragment_main, container, false);
+
+        items = new ArrayList<>();
+        misDias = (TextView) fragmento.findViewById(R.id.TVdias);
+        miLista = (ListView) fragmento.findViewById(R.id.LVmyList);
+        myAdapter = new ArrayAdapter <String> (getContext(), android.R.layout.simple_list_item_1, items);
+        miLista.setAdapter(myAdapter);
+        miLista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                myAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+        myAdapter.add("Monday 19/10/2015: Cloudy");
+        myAdapter.add("Tuesday 20/10/2015: Cloudy");
+        myAdapter.add("Wednesday 21/10/2015: Cloudy");
+        myAdapter.add("Thursday 22/10/2015: Cloudy");
+        myAdapter.add("Friday 23/10/2015: Cloudy");
+        myAdapter.add("Saturday 24/10/2015: Cloudy");
+        myAdapter.add("Sunday 25/10/2015: Cloudy");
+
+        return fragmento;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_fragment, menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
