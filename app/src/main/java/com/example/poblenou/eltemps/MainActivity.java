0201e57410f497032189e553f255a5bbc3720d7f
uri <48089748z@iespoblenou.org>
2015-10-21 20:41:53 +0200

    Segon commit del Projecte "Weather fet per mi (Oriol) Retrofit funcionant, seguent pas mostrar-ho a la ListView.

--- app/src/main/java/com/example/poblenou/eltemps/MainActivity.java
+++ app/src/main/java/com/example/poblenou/eltemps/MainActivity.java
@@ -43,7 +43,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
+
             return true;
         }
 
--- app/src/main/java/com/example/poblenou/eltemps/MainActivityFragment.java
+++ app/src/main/java/com/example/poblenou/eltemps/MainActivityFragment.java
@@ -1,20 +1,28 @@
 package com.example.poblenou.eltemps;
 
-import android.os.Bundle;
+import android.content.ClipData;
+import android.graphics.pdf.PdfRenderer;
+import android.os.*;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.*;
+import android.webkit.WebView;
+import android.widget.*;
+
+import com.example.poblenou.eltemps.json.Forecast;
+import com.example.poblenou.eltemps.json.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
+import retrofit.http.GET;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -25,6 +33,18 @@ public class MainActivityFragment extends Fragment
     private ArrayList<String> items;
     private ListView miLista;
     private TextView misDias;
+    private OpenWeatherMapService service;
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://api.openweathermap.org/data/2.5/") //URL DEL JASON DEL TIEMPO EN BARCELONA
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    public interface OpenWeatherMapService
+    {
+        @GET("forecast/city?id=3128760&APPID=f3b53a805bc9ec413f57d26fdc30de46")
+        Call<Forecast> getPrevisiones();
+    }
 
     public MainActivityFragment()
     {
@@ -38,24 +58,31 @@ public class MainActivityFragment extends Fragment
         setHasOptionsMenu(true);
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         View fragmento = inflater.inflate(R.layout.fragment_main, container, false);
+
+        service = retrofit.create(OpenWeatherMapService.class);
+        service.getPrevisiones();
 
         items = new ArrayList<>();
         misDias = (TextView) fragmento.findViewById(R.id.TVdias);
         miLista = (ListView) fragmento.findViewById(R.id.LVmyList);
-        myAdapter = new ArrayAdapter <String> (getContext(), android.R.layout.simple_list_item_1, items);
+        myAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, items);
         miLista.setAdapter(myAdapter);
         miLista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                items.remove(position);
-                myAdapter.notifyDataSetChanged();
+                //items.remove(position);
+                //myAdapter.notifyDataSetChanged();
                 return true;
             }
         });
+
         myAdapter.add("Monday 19/10/2015: Cloudy");
         myAdapter.add("Tuesday 20/10/2015: Cloudy");
         myAdapter.add("Wednesday 21/10/2015: Cloudy");
@@ -64,24 +91,55 @@ public class MainActivityFragment extends Fragment
         myAdapter.add("Saturday 24/10/2015: Cloudy");
         myAdapter.add("Sunday 25/10/2015: Cloudy");
 
+
+
+        //System.out.println(service.getPrevisiones().toString());
+
         return fragmento;
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
         super.onCreateOptionsMenu(menu, inflater);
 
         inflater.inflate(R.menu.menu_fragment, menu);
     }
 
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_refresh) {
+        if (id == R.id.action_refresh)
+        {
+            myAdapter.clear();
+            Call<Forecast> call = service.getPrevisiones();
+            call.enqueue(new Callback<Forecast>() {
+                @Override
+                public void onResponse(Response<Forecast> response, Retrofit retrofit) {
+                    Forecast forecast = response.body();
+                    for (List list : forecast.getList())
+                    {
+                        Long dt = list.getDt();
+                        String desc = list.getWeather().get(0).getDescription();
+                        Double min = list.getTemp().getMin();
+                        Double max = list.getTemp().getMax();
+
+                        //myAdapter.add(String.valueOf(Log.w("list", String.format("%s - %s - %s/%s", dt, desc, min, max))));
+                        //Esto aun no funciona
+
+                    }
+
+                }
+                @Override
+                public void onFailure(Throwable t) {
+
+                }
+            });
             return true;
         }
 
